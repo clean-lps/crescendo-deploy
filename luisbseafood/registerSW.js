aab4c95047b6b688742f65d8e33f5d1913828ae9
dnielpy <dnielquesada@gmail.com>
2026-08-13 10:29:30 -0400

    Deploy updated Luis B Sea Food build

--- luisbseafood/registerSW.js
+++ luisbseafood/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/crescendo-deploy/luisbseafood/sw.js', { scope: '/crescendo-deploy/luisbseafood/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
